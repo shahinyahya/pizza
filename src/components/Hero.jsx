f859cdf04520208cb97236e369072bfdaaf54723
shahinyahya <shahintyahya@gmail.com>
2023-02-04 20:53:12 +0530

    Created media queries on homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-head">
-        <h1>Garcia's Pizzeria</h1>
-      </div>
-      <div className="hero-sub">
-        <p>the best pizzas you ever had</p>
-      </div>
-      <div className="hero-btn">
-        <button>ORDER NOW</button>
+      <div className="hero-component">
+        <div className="hero-head">
+          <h1>Garcia's Pizzeria</h1>
+        </div>
+        <div className="hero-sub">
+          <p>the best pizzas you ever had</p>
+        </div>
+        <div className="hero-btn">
+          <button>ORDER NOW</button>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
+import { FaHamburger } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  if (toggleNav) {
+    document.body.style.overflow = "hidden";
+    document.querySelectorAll(".nav-menu ul li").forEach((el) => {
+      el.addEventListener("click", () => {
+        document.querySelector(".navbar").classList.add("active");
+      });
+    });
+  } else {
+    document.body.style.overflow = "scroll";
+  }
+
   return (
     <header className="header">
       <div className="logo-container">
         <img src={Logo} alt="pizza" width="60" />
       </div>
-      <div className="nav-menu">
+      <div onClick={() => setToggleNav(!toggleNav)} className="icon">
+        {toggleNav ? <AiOutlineClose /> : <FaHamburger />}
+      </div>
+      <div className={toggleNav ? "nav-menu navbar active" : "nav-menu navbar"}>
         <ul>
           <li>
             <Link className="link" to="/">
